0a57c6d69ac912189a22ffde4c70ab486496692a
“Seun <225109273+jubril-seun@users.noreply.github.com>
2026-08-17 23:40:19 -0400

    Write manual no skill javascript code to get 90 percent of calculator working

---
+++ script.js
@@ -0,0 +1,128 @@
+let input = document.getElementById("input-result");
+const btns = document.getElementById("calc-btns")
+
+input.value = "";
+let num1 = 0;
+let num2 = 0;
+let result = 0;
+let operator = null;
+let currInput = [];
+let operationActive = true;
+
+
+function addArr(val){
+    
+    for(let i = 0; i < val.length; i++){
+        currInput.push(val[i]);
+    }
+    
+    let newArr = currInput.join("");
+    
+    input.value = newArr;
+}
+
+
+function subtractArr(){
+    currInput.pop();
+    let newArr = currInput.join('');
+    
+    input.value = newArr;
+}
+
+
+function clearAll(){
+    input.value = "";
+    num1 = 0;
+    num2 = 0;
+    result = 0;
+    operator = null;
+    currInput = [];
+}
+
+function evaluate (){
+   
+    if(operator === "addition"){
+        result = num1 + num2;
+    } else if(operator === "subtraction"){
+        result = num1 - num2;
+    } else if(operator === "multiplication"){
+        result = num1 * num2;
+    } else if(operator === "division"){
+        result = num1 / num2;
+    }
+
+
+    if(Number.isInteger(result)){
+        input.value = result;
+        num1 = result; 
+    } else {
+        input.value = result.toFixed(2);
+        num1 = result.toFixed(2);
+    }
+    
+}
+
+btns.addEventListener("click", (e) => {
+      
+    let value = e.target.dataset.id;
+    
+    // Inputing numbers to display
+    if(e.target.classList.contains('number-btns')){   
+        if((input.value === "0"  || input.value === "") && value === "0"){
+            input.value = value;
+        } else if((input.value === "0" || input.value === "") && value === ".") {   
+            addArr("0");
+            addArr(".")
+        } else if(value === "." && currInput.includes(".")){
+            return;
+        } else {
+            addArr(value);
+        }
+    }
+
+    // Subtracting numbers from display
+    if(value === 'del'){
+        subtractArr()
+    }
+
+
+    if(value === "ac"){
+        clearAll();
+    }
+
+    if(e.target.classList.contains('operator-btn') && value !== "equals"){
+        operationActive = true;
+        operator = value;
+        num1 = Number(input.value);
+        currInput = [];
+        } else if (value === "equals" && num1 !== 0){
+            num2 = Number(input.value);
+            evaluate();
+            currInput = [];
+            addArr(input.value);
+            operationActive = false;
+    }
+
+    if(!operationActive && e.target.classList.contains('number-btns')){
+        input.value = "";
+        currInput = [];
+        if(value === "0"){
+            input.value = value;
+        } else if((input.value === "0" || input.value === "") && value === ".") {   
+            addArr("0");
+            addArr(".")
+        } else {
+            addArr(value);
+        }
+
+        operationActive = true;
+    }
+
+    console.log(operationActive);
+    console.log("result: " + result);
+    console.log("num1: " + num1);
+    console.log("num2: " + num2);
+    console.log("operator: " + operator)
+    console.log(currInput);
+    console.log(input.value);
+})
